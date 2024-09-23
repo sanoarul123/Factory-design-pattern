d9ec817f48077f8911b19acdef2832ba53e93859
sanoarul123 <59704648+sanoarul123@users.noreply.github.com>
2024-09-23 21:53:50 +0600

    first

---
+++ src/main/java/com/factorydesignpattern/demo/controller/PaymentController.java
@@ -0,0 +1,23 @@
+package com.factorydesignpattern.demo.controller;
+
+import com.factorydesignpattern.demo.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payment")
+public class PaymentController {
+
+
+    private PaymentService paymentService;
+
+    @PostMapping("/process")
+
+    public String processPayment(@RequestParam String gatewayType, @RequestParam double amount) {
+        System.out.println("Processing payment...");
+        return paymentService.processTransaction(gatewayType, amount);
+    }
+}
